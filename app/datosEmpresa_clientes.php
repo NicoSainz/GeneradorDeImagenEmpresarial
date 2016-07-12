6d8a3f080214239e11ef808b02c64fee433fed66
Nicolas <nicolas.sainz.pineda@gmail.com>
2016-07-12 11:39:40 -0600

    Se crean Modelos y Controlador

---
+++ app/datosEmpresa_clientes.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class datosEmpresa_clientes extends Model
+{
+    protected $table = 'datosEmpresa_clientes';
+}
---
+++ app/datos_empresa.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class datos_empresa extends Model
+{
+    protected $table = 'datosempresa';
+}
