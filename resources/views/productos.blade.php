349a333798e4db784125812f91dd15c180c3764f
alan <alanjes4@hotmail.com>
2016-07-19 23:56:54 -0600

    guardar descripcion de art y serv en BD

--- resources/views/productos.blade.php
+++ resources/views/productos.blade.php
@@ -1,6 +1,6 @@
 @extends('master')
 @section('encabezado')
-<h1>Productos</h1>
+<h1>Articulos & Servicios</h1>
 @stop
 
 @section('contenido')
@@ -13,77 +13,55 @@
 			<fieldset>
 			<form action="{{url('/guardarProductos')}}/{{$clientes->id}}" method="POST">
         	<input type="hidden" name="_token" value="{{csrf_token()}}">
+
 			  	<h3>Articulos :</h3>
-			    <div class="form-group">			      
-			      <div class="col-xs-6">
-			        <input value="{{$datos_empresa->articulo1}}" type="text" class="form-control" name="articulo1" placeholder="articulo #1">		        
-			      </div>
 
+			    <div class="form-group">
+			    	<div class="col-xs-4">	      
+			        	<input value="{{$datos_empresa->articulo1}}" type="text" class="form-control" name="articulo1" placeholder="articulo #1">
+			        </div>
+			        <div class="col-xs-8">
+			        	<input value="{{$datos_empresa->Darticulo1}}" type="text" class="form-control" name="Darticulo1" placeholder="Descripcion articulo #1">
+			        </div>
+			   	</div>
 
-<!--			      <form enctype="multipart/form-data" action="uploader.php" method="POST">
-			      <div class="col-xs-4">
-			      	<input name="uploadedfile" type="file" />			        			        
-			      </div>
-			      <div class="col-xs-2">
-			      	<input type="submit" value="CargarImagen" />			      	
-			      </div>	
-			      </form>
-			    </div>	
--->
-			    <div class="form-group">			      
-			      <div class="col-xs-6">
-			        <input value="{{$datos_empresa->articulo2}}" type="text" class="form-control" name="articulo2" placeholder="articulo #2"><hr>			        
-			      </div>
-<!--			      <form enctype="multipart/form-data" action="uploader.php" method="POST">
-			      <div class="col-xs-4">
-			      	<input name="uploadedfile" type="file" />			        			        
-			      </div>
-			      <div class="col-xs-2">
-			      	<input type="submit" value="CargarImagen" />			      	
-			      </div>	
-			      </form>
-			    </div>	
--->
-				<br>
-				<br>
+			   <div class="form-group">
+			    	<div class="col-xs-4">	      
+			        	<input value="{{$datos_empresa->articulo2}}" type="text" class="form-control" name="articulo2" placeholder="articulo #2">
+			        </div>
+			        <div class="col-xs-8">
+			        	<input value="{{$datos_empresa->Darticulo2}}" type="text" class="form-control" name="Darticulo2" placeholder="Descripcion articulo #2">
+			        </div>
+			   	</div>				
+				
 			    <h3>Servicios :</h3>
-			    <div class="form-group">			    
-			      <div class="col-xs-6">
-			        <input value="{{$datos_empresa->servicio1}}" type="text" class="form-control" name="servicio1" placeholder="servicio #1"> 		        
-			      </div>
-<!--			      <form enctype="multipart/form-data" action="uploader.php" method="POST">
-			      <div class="col-xs-4">
-			      	<input name="uploadedfile" type="file" />			        			        
-			      </div>
-			      <div class="col-xs-2">
-			      	<input type="submit" value="CargarImagen" />			      	
-			      </div>	
-			      </form>
-			    </div>	
--->
-			    <div class="form-group">			    
-			      <div class="col-xs-6">
-			        <input value="{{$datos_empresa->servicio2}}" type="text" class="form-control" name="servicio2" placeholder="servicio #2"><hr>			        
-			      </div>
-<!--			      <form enctype="multipart/form-data" action="uploader.php" method="POST">
-			      <div class="col-xs-4">
-			      	<input name="uploadedfile" type="file" />			        			        
-			      </div>
-			      <div class="col-xs-2">
-			      	<input type="submit" value="CargarImagen" />			      	
-			      </div>	
-			      </form>
-			    </div>	
--->
+			    <div class="form-group">
+			    	<div class="col-xs-4">	      
+			        	<input value="{{$datos_empresa->servicio1}}" type="text" class="form-control" name="servicio1" placeholder="servicio #1">
+			        </div>
+			        <div class="col-xs-8">
+			        	<input value="{{$datos_empresa->Dservicio1}}" type="text" class="form-control" name="Dservicio1" placeholder="Descripcion servicio #1">
+			        </div>
+			   	</div>
+
+			   <div class="form-group">
+			    	<div class="col-xs-4">	      
+			        	<input value="{{$datos_empresa->servicio2}}" type="text" class="form-control" name="servicio2" placeholder="servicio #2">
+			        </div>
+			        <div class="col-xs-8">
+			        	<input value="{{$datos_empresa->Dservicio2}}" type="text" class="form-control" name="Dservicio2" placeholder="Descripcion servicio #2">
+			        </div>
+			   	</div>
+
 			  </fieldset>
-			  <div class="container">
+			  	<div class="container">
                         <div class="row" align="center">
                             <br>
                             <a href="{{url('/mision_vision')}}/{{$clientes->id}}" class="btn btn-primary">Anterior</a>
                             <input align="center" type="submit" class="btn btn-primary" value="Guardar">
                             <a href="{{url('/descripcion')}}/{{$clientes->id}}" class="btn btn-primary">Siguiente</a>
                         </div>
-                    </div>
+              	</div>
                 </form>
 		</div>
 	</div>
